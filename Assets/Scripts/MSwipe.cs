27ee409e3f54c41e2da52a742e8100f1cde18cf9
Rahul Adke <rsadke007@gmail.com>
2024-08-20 02:07:50 +0530

    Add files via upload

---
+++ Assets/Scripts/MSwipe.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+
+public class MSwipe : MonoBehaviour {
+
+	private bool tap, swipeLeft,swipeRight, swipeUp,swipeDown;
+	private Vector2 startTouch,swipeDelta;
+	private bool isDragging =false;
+
+	private void Update()
+	{
+		tap = swipeLeft = swipeRight = swipeUp = swipeDown = false;
+
+		#region Standalone Inputs
+		if(Input.GetMouseButtonDown(0))
+		{
+			tap=true;
+			isDragging=true;
+			startTouch=Input.mousePosition;
+		}
+		else if(Input.GetMouseButtonUp(0))
+		{
+			isDragging=false;
+			Reset();
+		}
+		#endregion
+
+		#region Mobile Inputs
+		if(Input.touches.Length>0)
+		{
+			if(Input.touches[0].phase==TouchPhase.Began)
+			{
+				tap=true;
+				isDragging=true;
+				startTouch=Input.touches[0].position;
+			}else if(Input.touches[0].phase==TouchPhase.Ended ||Input.touches[0].phase==TouchPhase.Canceled)
+			{
+				isDragging=false;
+				Reset();
+			}
+		}
+		#endregion
+
+		//Calculate the Distance
+		swipeDelta=Vector2.zero;
+		if (isDragging) {
+			if (startTouch != Vector2.zero) {
+				if (Input.touches.Length > 0)
+					swipeDelta = Input.touches [0].position - startTouch;
+				else if (Input.GetMouseButton (0))
+					swipeDelta = (Vector2)Input.mousePosition - startTouch;
+			}
+		}
+
+		//Did we crossed the deadzone
+		if (swipeDelta.magnitude > 125) {
+			//which direction?
+			float x = swipeDelta.x;
+			float y = swipeDelta.y;
+			if (Mathf.Abs (x) > Mathf.Abs (y)) {
+				//Left or Right
+				if (x < 0)
+					swipeLeft = true;
+				else
+					swipeRight = true;
+
+			} else {
+				//Up or Down
+				if (y > 0)
+					swipeUp = true;
+				else
+					swipeDown = true;
+			}
+
+			Reset ();
+		}
+	}
+
+	private void Reset()
+	{
+		startTouch = swipeDelta = Vector2.zero;
+		isDragging = false;
+	}
+
+	public bool Tap{ get { return tap; } }
+	public Vector2 SwipeDelta{ get { return swipeDelta; } }
+	public bool SwipeLeft{ get { return swipeLeft; } }
+	public bool SwipeRight{ get { return swipeRight; } }
+	public bool SwipeUp{ get { return swipeUp; } }
+	public bool SwipeDown{ get { return swipeDown; } }
+}
